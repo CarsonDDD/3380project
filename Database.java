a34e9317ef6473a7f45b3f2e120e5f16bce935b8
CarsonDDD <carson-dickinson@hotmail.com>
2023-11-09 11:34:20 -0600

    Import database code

---
+++ Database.java
@@ -0,0 +1,75 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.function.Consumer;// May need to remove
+
+class Database {
+	private class ColumnValue{
+		public String column;
+		public Object value;
+
+		public ColumnValue(String columnName, Object value){
+			column = columnName;
+			this.value = value;
+		}
+	}
+
+	private Connection connection;
+
+	public Database(String url) {
+		try {
+			// create a connection to the database
+			connection = DriverManager.getConnection(url);
+		} catch (SQLException e) {
+			e.printStackTrace(System.out);
+		}
+	}
+
+	// Execute statment and use the operation on each entry in the result
+	private boolean executeQuery(PreparedStatement statment, Consumer<ResultSet> operation){
+		boolean success = false;
+		try{
+			ResultSet resultSet = statment.executeQuery();
+
+			// Check to see if resultset is not empty
+			if(resultSet.isBeforeFirst()){
+				success = true;
+				// For every returned entry, run the operation with it
+				// Normally a print, its up to the caller to determine what to do with this
+				while (resultSet.next()) {
+					operation.accept(resultSet);
+				}
+			}
+		}
+		catch(Exception e){
+			System.err.println(e.getMessage());
+		}
+		return success;
+	}
+
+	// Returns list of given columns with their values
+	private ColumnValue[] getColumnsValues(ResultSet rowSet, String[] columns, boolean showNullColumns){
+		ArrayList<ColumnValue> output = new ArrayList<>();
+
+		try{
+			for (String column : columns) {
+				Object value = rowSet.getObject(column);
+
+				// Check if we should append if an attribute is null
+				if(!(!showNullColumns && value == null)){
+					output.add(new ColumnValue(column, value));
+				}
+			}
+		}
+		catch(Exception e){
+			System.err.println(e.getMessage());
+		}
+
+		return output.toArray(new ColumnValue[0]);
+	}
+}
--- Program.java
+++ Program.java
@@ -8,11 +8,23 @@ import java.sql.SQLException;
 
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Scanner;
 import java.sql.PreparedStatement;
 
 public class Program {
+	static Connection connection;
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+	public static void main(String[] args) throws Exception {
+		Database db = new Database("jdbc:sqlite:library.db");
+		runConsole(db);
+
+		System.out.println("Exiting...");
 	}
+
+	public static void runConsole(Database db) {
+		Scanner console = new Scanner(System.in);
+		
+		console.close();
+	}
+
 }
