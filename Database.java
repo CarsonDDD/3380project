717356ceec5c6ab92603ae0a56a657472b50399a
CarsonDDD <carson-dickinson@hotmail.com>
2023-11-16 13:43:45 -0600

    Add old code

--- Database.java
+++ Database.java
@@ -2,44 +2,40 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
-import java.sql.Statement;
 import java.sql.SQLException;
 import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.function.Consumer;// May need to remove
 
-class Database {
-	private class ColumnValue{
-		public String column;
-		public Object value;
+import javafx.beans.binding.ObjectBinding;
 
-		public ColumnValue(String columnName, Object value){
-			column = columnName;
-			this.value = value;
-		}
+class ColumnValuePair{
+	public String column;
+	public Object value;
+
+	public ColumnValuePair(String columnName, Object value){
+		column = columnName;
+		this.value = value;
 	}
+}
 
+class Database {
 	private Connection connection;
 
-	public Database(String url) {
+	public Connection connect(String url){
 		try {
 			// create a connection to the database
 			connection = DriverManager.getConnection(url);
-		} catch (SQLException e) {
+		}
+		catch (SQLException e) {
 			e.printStackTrace(System.out);
 		}
+
+		return connection;
 	}
 
-	
-	/*
-		PreparedStatement statment = connection.prepareStatement(STRING sql);
-		statment.setString(1, names[0].toLowerCase());
-		statment.setString(2, names[1].toLowerCase());
-
-		exectureQuery(statment, (resultSet) -> {PER RESULT OPERATION});
-	*/
 	// Execute statment and use the operation on each entry in the result
-	public boolean executeQuery(PreparedStatement statment, Consumer<ResultSet> operation){
+	public boolean executeQuery(PreparedStatement statment, Consumer<ResultSetMetaData> operation){
 		boolean success = false;
 		try{
 			ResultSet resultSet = statment.executeQuery();
@@ -50,7 +46,7 @@ class Database {
 				// For every returned entry, run the operation with it
 				// Normally a print, its up to the caller to determine what to do with this
 				while (resultSet.next()) {
-					operation.accept(resultSet);
+					operation.accept(resultSet.getMetaData());
 				}
 			}
 		}
@@ -62,16 +58,19 @@ class Database {
 
 	// Maybe pulbic? ResultSet being public is problematic
 	// Returns list of given columns with their values
-	private ColumnValue[] getColumnsValues(ResultSet rowSet, String[] columns, boolean showNullColumns){
-		ArrayList<ColumnValue> output = new ArrayList<>();
+	// This function may be broken. It is untested. Ill make it fix later
+	public static ColumnValuePair[] getColumnValuesPair(ResultSetMetaData resultMeta, String[] columns, boolean showNullColumns){
+		ArrayList<ColumnValuePair> output = new ArrayList<>();
 
 		try{
 			for (String column : columns) {
-				Object value = rowSet.getObject(column);
+				//Object value = resultMeta.getObject(column);
+				Object value = resultMeta.get
+				// We need to getObject from the meta data
 
 				// Check if we should append if an attribute is null
 				if(!(!showNullColumns && value == null)){
-					output.add(new ColumnValue(column, value));
+					output.add(new ColumnValuePair(column, value));
 				}
 			}
 		}
@@ -79,6 +78,6 @@ class Database {
 			System.err.println(e.getMessage());
 		}
 
-		return output.toArray(new ColumnValue[0]);
+		return output.toArray(new ColumnValuePair[0]);// maybe to slow?
 	}
 }
--- Program.java
+++ Program.java
@@ -12,12 +12,17 @@ import java.util.Scanner;
 import java.sql.PreparedStatement;
 
 public class Program {
-	static Connection connection;
 
+	// Used as a reference to the database connection to run querys on it.
+	// Needed for prepared statment to avoid sql injection.
+	static Connection connection; 
+	static Database database;
 	public static void main(String[] args) throws Exception {
-		//Database db = new Database("jdbc:sqlite:library.db");
-		//runConsole(db);
-		//System.out.println("Exiting...");
+		database = new Database();
+		connection = database.connect("jdbc:sqlite:library.db");
+
+		runConsole(database);
+		System.out.println("Exiting...");
 
 		Debug.close(); // Required to be called at end of program
 	}
@@ -26,17 +31,33 @@ public class Program {
 		String input;
 
 		Scanner console = new Scanner(System.in);
-		
-
 
 		while((input = console.nextLine()) != null ) {
 			String[] args = input.split(" ");
 			String command = args[0];
 			
 			// Logic
-
-
+	
 		}
 		console.close();
 	}
+
+	public static void queryExample() throws SQLException{
+		/*
+		PreparedStatement statment = connection.prepareStatement(STRING sql);
+		statment.setString(1, names[0].toLowerCase());
+		statment.setString(2, names[1].toLowerCase());
+
+		exectureQuery(statment, (resultSet) -> {PER RESULT OPERATION});
+		*/
+		PreparedStatement statement = connection.prepareStatement("SELECT * FROM table WHERE thing = ?");
+		statement.setString(1, "string");
+
+		database.executeQuery(statement, (resultMeta) -> {
+			//database.get
+			//ColumnValuePair[] res = Database.getColumnValuesPair(resultMeta, new String[]{"name", "id", "age"}, false);
+
+			resultMeta.getColumnType("test");
+		});
+	}	
 }
