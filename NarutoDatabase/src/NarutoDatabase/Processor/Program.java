fb94f65ba1ebf03b6019b9567e872d7853ed779d
CarsonDDD <carson-dickinson@hotmail.com>
2023-11-30 13:45:46 -0600

    fix import

--- NarutoDatabase/src/NarutoDatabase/Processor/Program.java
+++ NarutoDatabase/src/NarutoDatabase/Processor/Program.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.sql.PreparedStatement;
-import NarutoDatabase.Logger.Debug;
+import NarutoDatabase.Logger.Output;
 
 public class Program {
 
